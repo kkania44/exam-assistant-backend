6b77660689e20888b2c06867142bbecf93c81461
Krzysztof Kania <kaniakrzysztof99@gmail.com>
2024-06-30 23:23:17 +0200

    init spring boot app

---
+++ src/main/java/com/example/exam_assistant/ExamAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exam_assistant/ExamAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
